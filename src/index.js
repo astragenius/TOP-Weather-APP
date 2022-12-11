7bd2c895d5ce2b504f82c1f1b94cdaa256a4f79b
astragenius <darthmalak86@gmail.com>
2022-12-11 21:18:54 +0100

    add - added html struckture

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './scss/index.scss'
+import img from './assets/img/sun rain.svg'
+import img1 from './assets/img/rain.svg'
+
+const imgs = document.getElementById('img')
+const weatherImg = [...document.querySelectorAll('.weather-img')]
+imgs.src = img
+
+weatherImg.forEach((el) => {
+    el.src = img1
+})
