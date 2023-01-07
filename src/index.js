3faf28b524d29df6b4f31729a37d739328ce975a
astragenius <darthmalak86@gmail.com>
2023-01-07 21:13:43 +0100

    add - some bugfixes

--- src/index.js
+++ src/index.js
@@ -11,22 +11,6 @@ imgs.src = img
 weatherImg.forEach((el) => {
     el.src = img1
 })
-/* searchBtn.addEventListener('click', () => {
-    const weatherData = fetch(
-        'https://api.open-meteo.com/v1/forecast?latitude=49.42&longitude=6.72&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&timezone=auto&start_date=2023-01-07&end_date=2023-01-12',
-        { mode: 'cors' }
-    )
-    weatherData.then((response, reject) => {
-        console.log(response.json())
-    })
-
-    const cityName = searchInput.value
-    const geoAPI = ''
-    const cityCords = fetch(geoAPI, { mode: 'cors' })
-    cityCords.then((response, reject) => {
-        console.log(response.json())
-    })
-}) */
 
 function getInput() {
     const searchInput = document.getElementById('search-input').value
@@ -59,7 +43,6 @@ async function getCords(cityName) {
 async function getCity(coords) {
     const lon = coords.lon
     const lat = coords.lat
-    console.log(lon)
     const response = fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&timezone=auto&start_date=2023-01-07&end_date=2023-01-12`,
         { mode: 'cors' }
